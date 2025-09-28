9d6d4ff991f49126d485f5b0ffbe5b71ab88bd2a
checkitsedo <info@senti.lu>
2025-09-28 13:08:06 +0200

    Removed files: winpe-script/osdcloud-archive

--- winpe-script/osdcloud-archive/PSModule OSD Copy to WinPE.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host -ForegroundColor DarkGray "[$(Get-Date -format G)] Copy PowerShell Module OSD to BootImage"
-
-Copy-PSModuleToWindowsImage -Name 'OSD' -Path $MountPath
--- winpe-script/osdcloud-archive/PSModule OSD Save to WinPE.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host -ForegroundColor DarkGray "[$(Get-Date -format G)] Save PowerShell Module OSD to BootImage"
-
-Save-Module -Name OSD -Path "$MountPath\Program Files\WindowsPowerShell\Modules" -Force
